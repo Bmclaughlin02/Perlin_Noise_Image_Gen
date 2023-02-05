ed605a30450bc4ce4e03ebee3b0953e7750314b2
BrennanMcl <bdmclaughlin02@gmail.com>
2023-02-05 17:01:22 -0500

    Completed random Perlin noise

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import png
+from noise2d import noise2d
+import random
 
-png.from_array([[255, 0, 0, 255],[0, 255, 255, 0]], 'L').save("small_smiley.png")
+width = 500
+height = 500
+img = []
+P = list(range(0,256))
+random.shuffle(P)
+
+for y in range(height):
+    row = ()
+    for x in range(width):
+        n = noise2d(x*.01, y*.01, P)
+        n += 1.0
+        n /= 2.0
+
+        color = round(int(255*n), 0)
+
+        row = row + (color, color, color)
+    img.append(row)
+
+with open('noise.png', 'wb') as f:
+    w = png.Writer(width, height, greyscale=False)
+    w.write(f, img)
---
+++ noise2d.py
@@ -0,0 +1,57 @@
+from math import floor
+
+class vec2:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+def dotp(self: vec2, other: vec2):
+        return self.x*other.x + self.y*other.y
+
+def getConstantVector(v):
+    h = v & 3
+    if(h == 0):
+        a = vec2(1.0, 1.0)
+    elif(h == 1):
+        a = vec2(-1.0, 1.0)
+    elif(h == 2):
+        a = vec2(-1.0, -1.0)
+    else:
+        a = vec2(1.0, -1.0)
+
+    return a
+
+def smoothStepFade(t):
+    return t*t*t*((t*6.0 - 15.0)*t + 10)
+
+def lerp(t, a1, a2):
+    return a1 + t*(a2-a1)
+
+def noise2d(x, y, P):
+    Permutations = P
+
+    X = floor(x) & 255
+    Y = floor(y) & 255
+
+    xf = x-floor(x)
+    yf = y-floor(y)
+
+    topRight = vec2(xf-1.0, yf-1.0)
+    topLeft = vec2(xf, yf-1.0)
+    bottomRight = vec2(xf-1.0, yf)
+    bottomLeft = vec2(xf, yf)
+
+    valueTopRight = P[P[X+1]+Y+1]
+    valueTopLeft = P[P[X]+Y+1]
+    valueBottomRight = P[P[X+1]+Y]
+    valueBottomLeft = P[P[X]+Y]
+
+    dotTopRight = dotp(topRight, getConstantVector(valueTopRight))
+    dotTopLeft = dotp(topLeft, getConstantVector(valueTopLeft))
+    dotBottomRight = dotp(bottomRight, getConstantVector(valueBottomRight))
+    dotBottomLeft = dotp(bottomLeft, getConstantVector(valueBottomLeft))
+
+    u = smoothStepFade(xf)
+    v = smoothStepFade(yf)
+
+    return lerp(u, lerp(v, dotBottomLeft, dotTopLeft), lerp(v, dotBottomRight, dotTopRight))
