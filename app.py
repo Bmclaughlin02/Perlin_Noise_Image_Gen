ae85cdfb9f64d704eac5080cee707fa365d81cca
BrennanMcl <bdmclaughlin02@gmail.com>
2023-02-15 18:27:00 -0500

    Added extra colors to generator

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ random.shuffle(P)
 for y in range(height):
     row = ()
     for x in range(width):
-        n = 0.0
-        a = 1.0
+        n = 0.1
+        a = 1.5
         f = .005
-        for o in range(3):
+        for o in range(4):
             v = a*noise2d(x*f, y*f, P, reps)
             n += v
             a *= 0.5
@@ -30,7 +30,10 @@ for y in range(height):
         elif (color < 0):
             color = 0
 
-        row = row + (color, color, color)
+        if (color >= 100):
+            row = row + (0, 0, color)
+        else:
+            row = row + (0, color, 0)
     img.append(row)
 
 with open('noise.png', 'wb') as f:
